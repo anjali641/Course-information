c75867fc057f9b061cfbcf3e35dac14c970b0ee2
anjali641 <ak4373652@gmail.com>
2026-03-05 20:30:56 +0530

    update

--- js/formSubmit.js
+++ js/formSubmit.js
@@ -5,23 +5,27 @@ contactForm.addEventListener("submit", function(e) {
 e.preventDefault();
 
 const data = {
+
 name: document.getElementById("name").value,
 email: document.getElementById("email").value,
 phone: document.getElementById("phone").value,
-message: document.getElementById("message").value
+education: document.getElementById("education").value,
+class: document.getElementById("class")?.value || "",
+inter: document.getElementById("inter")?.value || "",
+grad_subject: document.getElementById("grad_subject")?.value || "",
+master_subject: document.getElementById("master_subject")?.value || "",
+other: document.getElementById("other")?.value || ""
+
 };
 
 fetch("https://script.google.com/macros/s/AKfycbzZRfVrdyyx16qsa6GE18aZkqKVAl8qNX52qSnQv2X1tLLmZkZQl2-Twmz_1rX5Qi1_/exec", {
 method: "POST",
 mode: "no-cors",
 body: JSON.stringify(data)
-})
-.then(() => {
+});
 
 document.getElementById("formSuccess").style.display = "block";
 
 contactForm.reset();
-
-});
 
 });
